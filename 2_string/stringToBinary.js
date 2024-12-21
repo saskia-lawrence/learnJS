07663bcd1ea750a5f1f9ea3119b2e769060036e8
sakia-lawrence <saskialawrence02@gmail.com>
2024-12-21 17:18:48 +0000

    prettified

--- 2_string/stringToBinary.js
+++ 2_string/stringToBinary.js
@@ -1,20 +1,16 @@
-let a ='apple'
-let stringToBinary =function(b)
-{
-    let e = ''
-    for(let i in b)
-    {
-        let c = b[i]
-        let d = c==='a'||c==='e'||c==='i'||c==='o'||c==='u'
-        if(d)
-        {
-e=e+0
-        }
-        else
-        {
-            e=e+1
-        }
-    }return e
-}
-let t = stringToBinary(a)
-console.log(t)
+let a = "apple";
+let stringToBinary = function (b) {
+  let e = "";
+  for (let i in b) {
+    let c = b[i];
+    let d = c === "a" || c === "e" || c === "i" || c === "o" || c === "u";
+    if (d) {
+      e = e + 0;
+    } else {
+      e = e + 1;
+    }
+  }
+  return e;
+};
+let t = stringToBinary(a);
+console.log(t);
