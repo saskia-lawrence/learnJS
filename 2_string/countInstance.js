d63b180e3881b18a3e640e57792417eb8dcee9ea
saskia-lawrence <saskialawrence02@gmail.com>
2024-12-22 06:52:36 +0000

    test

---
+++ 2_string/countInstance.js
@@ -0,0 +1,13 @@
+let a = "abcOKdefOK";
+let countOK = function (b) {
+  let d = 0;
+  for (let i = 0; i <= a.length; i++) {
+    let c = b[i] + b[i + 1];
+    if (c === "OK") {
+      d = d + 1;
+    }
+  }
+  return d;
+};
+let e = countOK(a);
+console.log(e);
--- 2_string/parseAlphanumeric.js
+++ 2_string/parseAlphanumeric.js
@@ -1,30 +1,24 @@
-let a = 'ab12cdnm56k78'
-let isNumber = function(str)
-{
-    return !isNaN(Number(str));
-}
-let isAlphabet = function(str)
-{
-    return /^[a-zA-z]+$/.test(str);
-}
+let a = "ab12cdnm56k78";
+let isNumber = function (str) {
+  return !isNaN(Number(str));
+};
+let isAlphabet = function (str) {
+  return /^[a-zA-z]+$/.test(str);
+};
 
-let parseAlphaNumeric = function(B)
-{
-    let e =''
-    let f =''
-    for(let i in B)
-    {
-        let d= B[i]
-        if(isNumber(d))
-        {
-            e=e+d
-        }
-        if(isAlphabet(d))
-        {
-            f=f+d
-        }
-
-    }return[e,f]
-}
-let s = parseAlphaNumeric(a)
-console.log(s[0],s[1])
+let parseAlphaNumeric = function (B) {
+  let e = "";
+  let f = "";
+  for (let i in B) {
+    let d = B[i];
+    if (isNumber(d)) {
+      e = e + d;
+    }
+    if (isAlphabet(d)) {
+      f = f + d;
+    }
+  }
+  return [e, f];
+};
+let s = parseAlphaNumeric(a);
+console.log(s[0], s[1]);
