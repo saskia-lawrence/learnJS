3525376763f15aea22b6506b013456637994a980
saskia-lawrence <saskialawrence02@gmail.com>
2024-12-29 08:47:37 +0000

    array2Object

--- 4_object/array2Object.js
+++ 4_object/array2Object.js
@@ -1,5 +1,5 @@
 let A = [
-  "apple",
+  "Apple",
   "banana",
   "carrot",
   "apricot",
@@ -14,11 +14,11 @@ let B = {
 };
 for (let i = 0; i < A.length; i++) {
   let C = A[i];
-  if (C[0] === "a") {
+  if (C[0] === "a" || C[0].toUpperCase() === "A") {
     B.a.push(C);
-  } else if (C[0] === "b") {
+  } else if (C[0] === "b" || C[0].toUpperCase() === "B") {
     B.b.push(C);
-  } else if (C[0] === "c") {
+  } else if (C[0] === "c" || C[0].toUpperCase() === "C") {
     B.c.push(C);
   }
 }
