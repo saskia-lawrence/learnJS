691a415ed30e9864bdbadb59ac135b4d78477d28
saskia-lawrence <saskialawrence02@gmail.com>
2025-01-28 13:42:01 +0000

    verifyEmail

--- 2_string/verifyEmail.js
+++ 2_string/verifyEmail.js
@@ -1,10 +1,9 @@
-let a = 'sam@12.in'
-let b = a.split('@')
-let c = b[1].split('.')
+let a = "sam@12.in";
+let b = a.split("@");
+let c = b[1].split(".");
 //console.log(c)
-if(b[0].length>0&&c[0].length>0&&c[1].length>0){
-console.log('true')
-}
-else{
-    console.log('false')
+if (b[0].length > 0 && c[0].length > 0 && c[1].length > 0) {
+  console.log("true");
+} else {
+  console.log("false");
 }
