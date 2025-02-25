1114840303446647ef9d04b579e12b9754ff65a9
saskia-lawrence <saskialawrence02@gmail.com>
2025-02-25 21:34:13 +0000

    state_1.jsx

---
+++ 6_react/state/1_concept/state_1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [toggle, setToggle] = useState();
+
+  return (
+    <div>
+      <p>The toggle is {toggle}</p>
+      <button onClick={() => setToggle("On")}>On</button>
+      <button onClick={() => setToggle("Off")}>Off</button>
+    </div>
+  );
+
+}
+
+export default App;
