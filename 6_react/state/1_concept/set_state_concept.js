8e94e1334df1d3c29cba4bd1d039f99ea518088c
saskia-lawrence <saskialawrence02@gmail.com>
2025-02-28 18:58:55 +0000

    1_concept/set_state_concept

--- 6_react/state/1_concept/set_state_concept.js
+++ 6_react/state/1_concept/set_state_concept.js
@@ -14,7 +14,7 @@ let [count, setCount] = useState(0);
  
   const increment = () => {
    
-    setCount((a)=>{
-      return a+1
+    setCount((a)=>{//this a has current value of count
+      return a+1 // this return  value will become new value of count 
     });
   }
