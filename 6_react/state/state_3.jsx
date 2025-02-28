bf44ad1db17dc0c26d03cf6f9ffecdec8da1864c
saskia-lawrence <saskialawrence02@gmail.com>
2025-02-28 13:26:23 +0000

    state_3.jsx

--- 6_react/state/state_3.jsx
+++ 6_react/state/state_3.jsx
@@ -3,26 +3,26 @@ import React, { useRef, useState } from "react";
 import "./style.css";
 
 const App = () => {
-  let [a, setA] = useState(1);
- 
-  let eventHandler = function (event) {
-    let b = event.target.innerHTML;
-    alert(b);
-        setA(()=>{
-      return b
-    });
-  };
-  
-  return (
-    <div>
-      <p>Wow, you've clicked that button:{a} </p>
-      <button onClick={eventHandler}>1</button>
-      <button onClick={eventHandler}>2</button>
-      <button onClick={eventHandler}>3</button>
-      <button onClick={eventHandler}>4</button>
-      <button onClick={eventHandler}>5</button>
-    </div>
-  );
-};
-
-export default App;
+    let [a, setA] = useState(1);
+    let [c, setC] = useState(1);
+     let eventHandler = function (event) {
+       let b = event.target.innerHTML;
+       alert(b);
+           setA(()=>b);
+       setC(()=>b*b);
+     };
+     
+     return (
+       <div>
+         <p>Wow, you've clicked that button:{a}, and its sq is {c} </p>
+         <button onClick={eventHandler}>1</button>
+         <button onClick={eventHandler}>2</button>
+         <button onClick={eventHandler}>3</button>
+         <button onClick={eventHandler}>4</button>
+         <button onClick={eventHandler}>5</button>
+       </div>
+     );
+   };
+   
+   export default App;
+   
