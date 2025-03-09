08013a6cc1559c42be6ea9e251217ac0016ba498
saskia-lawrence <saskialawrence02@gmail.com>
2025-03-09 17:33:09 +0000

    cheatSheet/js/string/ways_to_iterate_string.js file added

--- cheatSheet/js/string/stringFunction.js
+++ cheatSheet/js/string/stringFunction.js
@@ -1,30 +1,40 @@
 //toUpperCase()
 let A = "xyz".toUpperCase();
 console.log(A);
+
+
 //toLowerCase()
 let B = "XYZ".toLowerCase();
 console.log(B);
+
+
 //includes()
 let C = A.includes("X");
 console.log(C);
+
+
 //split()
 let D = A.split("");
 console.log(D);
 let E = "abc,def".split(",");
 console.log(E);
+
+
 //trim()
 let F = " abc ".trim();
 console.log(F);
+
+
 //substring()
 let G = "saskia";
 let H = G.substring(3);
 console.log(H);
-
 let J = G.substring(1, 3);
 console.log(J);
+
+
 //startswith(), endswith()
 let K = "saskia".startsWith("kia");
 console.log(K);
-
 let L = "saskia".endsWith("kia");
 console.log(L);
