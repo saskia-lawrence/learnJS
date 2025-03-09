0da44dec963d387dade62720ba9b5e09ad9a9073
saskia-lawrence <saskialawrence02@gmail.com>
2025-03-09 21:00:28 +0000

    cheatSheet/js/function/window_function.js

---
+++ cheatSheet/js/function/window_function.js
@@ -0,0 +1,11 @@
+setInterval(()=>{
+    console.log('ggv');
+  },5000)
+  // above function prints the string 'ggv' every 5 sec forever.
+
+
+
+  //=========
+
+  alert('hii') 
+  // shows the given string in alert dialog. 
