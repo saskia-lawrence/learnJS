8a08fd91ab14545233a8c33fa9ba73dca26bcca7
saskia-lawrence <saskialawrence02@gmail.com>
2025-03-12 17:36:02 +0000

    file modified

--- cheatSheet/js/function/window_function.js
+++ cheatSheet/js/function/window_function.js
@@ -2,10 +2,19 @@ setInterval(()=>{
     console.log('ggv');
   },5000)
   // above function prints the string 'ggv' every 5 sec forever.
-
+  //setInterval - First parameter - Callback, Second parameter - time in milliseconds (so 4000 means 4 seconds)
 
 
   //=========
 
   alert('hii') 
-  // shows the given string in alert dialog. 
+  // shows the given string in alert dialog.
+  
+
+   //=========
+  
+   setTimeout(()=>{
+    console.log('hello');
+  },5000)
+  // above function prints the string 'hello' after 5 secs (for only once)
+  //setTimeout - First parameter - Callback, Second parameter - time in milliseconds (so 4000 means 4 seconds)
