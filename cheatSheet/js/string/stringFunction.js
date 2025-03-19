2b2c0e1f5e18502bc460068c721113bfeb4a1661
saskia-lawrence <saskialawrence02@gmail.com>
2025-03-19 18:05:39 +0000

    string/stringFunction.js

--- cheatSheet/js/string/stringFunction.js
+++ cheatSheet/js/string/stringFunction.js
@@ -1,40 +1,40 @@
 //toUpperCase()
 let A = "xyz".toUpperCase();
-console.log(A);
+console.log(A);//XYZ
 
 
 //toLowerCase()
 let B = "XYZ".toLowerCase();
-console.log(B);
+console.log(B);//xyz
 
 
 //includes()
 let C = A.includes("X");
-console.log(C);
+console.log(C);//true
 
 
 //split()
 let D = A.split("");
-console.log(D);
+console.log(D);//['X','Y','Z']
 let E = "abc,def".split(",");
-console.log(E);
+console.log(E);//['abc', 'def']
 
 
 //trim()
 let F = " abc ".trim();
-console.log(F);
+console.log(F);//abc
 
 
 //substring()
 let G = "saskia";
 let H = G.substring(3);
-console.log(H);
+console.log(H);//kia
 let J = G.substring(1, 3);
-console.log(J);
+console.log(J);//as
 
 
 //startswith(), endswith()
 let K = "saskia".startsWith("kia");
-console.log(K);
+console.log(K);//false
 let L = "saskia".endsWith("kia");
-console.log(L);
+console.log(L);//true
