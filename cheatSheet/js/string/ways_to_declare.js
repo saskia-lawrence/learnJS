f8c22b4ff9e2a1dbb84c9c2be5874866535a0249
saskia-lawrence <saskialawrence02@gmail.com>
2025-03-23 13:39:34 +0000

    ways_to_iterate_string.js

--- cheatSheet/js/string/ways_to_declare.js
+++ cheatSheet/js/string/ways_to_declare.js
@@ -1,4 +1,24 @@
+//Way 1: Surrrounded by single or double quotes
 let a = "hello";
-let b = "abc";
+let K = "acv\ndef"; // '\n' means next line
+console.log(K);
+let Q = "qwe\tmnb"; // '\t' means tab
+console.log(Q);
+let Z = "ddd" + a + "lllll"; //dynamic string
+console.log(Z);
+
+//------------------------------------------------------
+
+//Way 2: Surrrounded by ``
 let c = `mind`;
-let d = `abc def  ghi  lmn tuv       154`;
+//muliple lines and tabs are allowed within the string declaration
+let d = `abc def  
+                ghi  
+         lmn tuv       
+                  13              xyz
+         154`;
+console.log(d);
+
+let M = `abccc${c}pppp
+            ppp`;
+console.log(M);
--- cheatSheet/js/string/ways_to_iterate_string.js
+++ cheatSheet/js/string/ways_to_iterate_string.js
@@ -4,3 +4,9 @@ for (let i = 0; i < a.length; i++) {
   let b = a[i];
   console.log(b);
 }
+
+//-------------------------------
+
+for (const c of a) {
+  console.log(c)
+}
